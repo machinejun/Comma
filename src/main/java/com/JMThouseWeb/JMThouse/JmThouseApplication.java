a4289a991eb879a1157c972483a8923f845d9fac
machinejun <akswns03@naver.com>
2022-07-05 16:59:54 +0900

    템플릿 결정

---
+++ src/main/java/com/JMThouseWeb/JMThouse/JmThouseApplication.java
@@ -0,0 +1,13 @@
+package com.JMThouseWeb.JMThouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmThouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JmThouseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/JMThouseWeb/JMThouse/JmThouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JMThouseWeb.JMThouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JmThouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
